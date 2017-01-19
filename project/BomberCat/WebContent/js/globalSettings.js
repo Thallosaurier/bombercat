af98705e14b0acde8852b05f5bb917090e2ceae0
Jonas <de.jonas.knapp@gmail.com>
2017-01-19 23:28:36 +0100

    Update globalSettings.js

--- project/BomberCat/WebContent/js/globalSettings.js
+++ project/BomberCat/WebContent/js/globalSettings.js
@@ -1,7 +1,7 @@
 var logServerMessages = false;
 var logSendMessages = false;
 
-var serverIP = "endlessarchitects.de";
+var serverIP = "endlessarchitects.de:8000";
 //var serverIP = "172.16.53.248";
 
 var mainColorR = 204;
